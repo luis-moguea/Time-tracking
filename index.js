5e100d77499e0886e77001f0b54f31f3e85e6742
luis-moguea <luis.mogueadp@gmail.com>
2022-09-13 18:35:03 -0500

    html updates using javascript

--- index.js
+++ index.js
@@ -3,60 +3,60 @@ const DATA = {
         {
         words_before: 'Report for',
         name: 'Jeremy Robson',
-        main_svg: 'img/image-jeremy.png',
+        main_svg: '/IMAGES/image-jeremy.png',
         color: 'smooth-blue',
         time_interval: [
             'Daily',
             'Weekly',
             'Monthly',
-        ]
+            ]
         }
     ],
 
     topic_cards: [
         {
             topic_name: 'Work',
-            ellipis_svg: 'img/ellipsis-work.svg',
+            ellipis_svg: '/IMAGES/icon-ellipsis.svg',
             topic_color: 'orange',
-            topic_icon_svg: '/img/icon-work.svg',
+            topic_icon_svg: '/IMAGES/icon-work.svg',
             time_hours: '32hrs',
             last_session_hours: 'Last week - 36hrs',
         },
         {
             topic_name: 'Play',
-            ellipis_svg: 'img/ellipsis-work.svg',
+            ellipis_svg: '/IMAGES/icon-ellipsis.svg',
             topic_color: 'blue',
-            topic_icon_svg: '/img/icon-play.svg',
+            topic_icon_svg: '/IMAGES/icon-play.svg',
             time_hours: '10hrs',
             last_session_hours: 'Last week - 8hrs',
         },
         {
             topic_name: 'Study',
-            ellipis_svg: 'img/ellipsis-work.svg',
+            ellipis_svg: '/IMAGES/icon-ellipsis.svg',
             topic_color: 'cherry',
-            topic_icon_svg: '/img/icon-study.svg',
+            topic_icon_svg: '/IMAGES/icon-study.svg',
             time_hours: '4hrs',
             last_session_hours: 'Last week - 7hrs',
         },{
             topic_name: 'Exercise',
-            ellipis_svg: 'img/ellipsis-work.svg',
+            ellipis_svg: '/IMAGES/icon-ellipsis.svg',
             topic_color: 'green',
-            topic_icon_svg: '/img/icon-exercise.svg',
+            topic_icon_svg: '/IMAGES/icon-exercise.svg',
             time_hours: '4hrs',
             last_session_hours: 'Last week - 5hrs',
         },{
             topic_name: 'Social',
-            ellipis_svg: 'img/ellipsis-work.svg',
+            ellipis_svg: '/IMAGES/icon-ellipsis.svg',
             topic_color: 'purple',
-            topic_icon_svg: '/img/icon-social.svg',
+            topic_icon_svg: '/IMAGES/icon-social.svg',
             time_hours: '5hrs',
             last_session_hours: 'Last week - 10hrs',
         },
         {
             topic_name: 'Self Care',
-            ellipis_svg: 'img/ellipsis-work.svg',
+            ellipis_svg: '/IMAGES/icon-ellipsis.svg',
             topic_color: 'yellow',
-            topic_icon_svg: '/img/icon-self-care.svg',
+            topic_icon_svg: '/IMAGES/icon-self-care.svg',
             time_hours: '2hrs',
             last_session_hours: 'Last week - 2hrs',
         },
@@ -73,18 +73,14 @@ const renderCards = () => {
         const mainCard = `
         <div class="grid__item--1">
             <div class="grid__item--2 ${el.color}">
-                <img src="${el.main_svg}">
+                <img class="grid__main-png" src="${el.main_svg}">
                 <p>${el.words_before}</p>
-                <p>${el.name}</p>
+                <h1>${el.name}</h1>
             </div>
             <div class="grid__item--3">
-                <ul>
-                    <li>
-                        <p class="interval__list>${el.time_interval[0]}</p><br>
-                        <p class="interval__list>${el.time_interval[1]}</p><br>
-                        <p class="interval__list>${el.time_interval[2]}</p>
-                    </li>
-                </ul>
+                <p class="interval__list>Daily</p>
+                <p class="interval__list>Weekly</p>
+                <p class="interval__list>Monthly</p>
             </div>
         </div>`;
         container.innerHTML += mainCard
@@ -93,14 +89,18 @@ const renderCards = () => {
 
     DATA.topic_cards.map(el => {
         const element = `
-        <div class="grid__item ${el.topic_color}">
-            <div class="grid__item--top-container">
-                <img src=${el.topic_icon_svg}>
+        <div class="grid__item">
+            <div class="grid__item--top-container ${el.topic_color}">
+                <img class="grid__item--svg" src=${el.topic_icon_svg}>
             </div>
             <div class="grid__item--bottom-container">
-                <div >
-                    <p>${el.topic_name}</p>
-                    <img src=${el.ellipis_svg}>
+                <div class="grid__item--bottom-1">
+                    <p class="grid__item--bottom-name"> ${el.topic_name}</p>
+                    <img class="grid__item--ellipsis" src=${el.ellipis_svg}>
+                </div>
+                <div "grid__item--bottom-2">
+                    <h1>${el.time_hours}</h1>
+                    <p class="grid__item--last">${el.last_session_hours}</p>
                 </div>
             </div>
         </div>`;
